f9c541260ebbeb7855a27aee5b2a7af7974a3942
SHIYA KEITA <keita.shiya@gmail.com>
2013-04-02 10:57:17 +0900

    250

--- Coredata-sample/Coredata-sample/DetailViewController.h
+++ Coredata-sample/Coredata-sample/DetailViewController.h
@@ -7,10 +7,18 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "Person.h"
 
 @interface DetailViewController : UIViewController <UISplitViewControllerDelegate>
 
-@property (strong, nonatomic) id detailItem;
+@property (strong, nonatomic) Person *detailItem;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (weak, nonatomic) IBOutlet UITextField *nameField;
+@property (weak, nonatomic) IBOutlet UITextField *zipCodeField;
+@property (weak, nonatomic) IBOutlet UITextField *stateField;
+@property (weak, nonatomic) IBOutlet UITextField *cityField;
+@property (weak, nonatomic) IBOutlet UITextField *otherField;
 
 @property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
 @end
